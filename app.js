dc3ebac244c78d2ae39450c79837a11cfb848c01
Deepali <deep.deepali05@gmail.com>
2022-02-03 16:12:19 +0530

    update:added another way of logic

--- app.js
+++ app.js
@@ -7,26 +7,41 @@ var Notes = [2000, 500, 100, 20, 10, 5, 1];
 
 // console.log(BillAmountis.value);
 
-function BillOnchangeHandler() {
-    //  console.log(BillAmountis.value);
-    if (BillAmountis.value < 0) {
-        errormsg.innerHTML = 'Invalid amount!';
-    } else {
-        errormsg.innerHTML = '';
-    }
-}
+// function BillOnchangeHandler() {
+//     //  console.log(BillAmountis.value);
+//     if (BillAmountis.value < 0) {
+//         errormsg.innerHTML = 'Invalid amount!';
+//     } else {
+//         errormsg.innerHTML = '';
+//     }
+// }
 
-function CashOnchangeHandler() {
-    if (parseInt(CashAmountis.value) < parseInt(BillAmountis.value)) {
-        errormsg.innerHTML = 'Cash amount should be greater than or equal to bill amount';
-    } else {
-        //  console.log(CashAmountis.value);
-        errormsg.innerHTML = '';
-    }
-}
+// function CashOnchangeHandler() {
+//     if (parseInt(CashAmountis.value) < parseInt(BillAmountis.value)) {
+//         errormsg.innerHTML = 'Cash amount should be greater than or equal to bill amount';
+//     } else {
+//         //  console.log(CashAmountis.value);
+//         errormsg.innerHTML = '';
+//     }
+// }
 
 function OnclickHandler() {
-    var amount = parseInt(CashAmountis.value) - parseInt(BillAmountis.value);
+    errormsg.innerHTML = '';
+    if (BillAmountis.value > 0) {
+        if (parseInt(CashAmountis.value) >= parseInt(BillAmountis.value)) {
+            var amount = parseInt(CashAmountis.value) - parseInt(BillAmountis.value);
+            Calculate(amount);
+        } else {
+            errormsg.innerHTML = 'Cash amount should be greater than or equal to bill amount';
+        }
+        
+    } else {
+        errormsg.innerHTML = 'Invalid amount!';
+    }
+}
+
+    function Calculate(amount) {
+    // var amount = parseInt(CashAmountis.value) - parseInt(BillAmountis.value);
     for (var i = 0; i < Notes.length; i++) {
         reqnotes[i].innerHTML = Math.trunc(amount / Notes[i]);
         amount = amount % Notes[i];
@@ -34,6 +49,6 @@ function OnclickHandler() {
     }
 }
 
-BillAmountis.addEventListener('change', BillOnchangeHandler);
-CashAmountis.addEventListener('change', CashOnchangeHandler);
+// BillAmountis.addEventListener('change', BillOnchangeHandler);
+// CashAmountis.addEventListener('change', CashOnchangeHandler);
 Clickbtnis.addEventListener('click', OnclickHandler);
